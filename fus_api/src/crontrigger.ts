b485217c085c43cd80f92924b2bdecfb19e972e0
Sean Feiner <75604516+sameghosts@users.noreply.github.com>
2023-08-23 22:16:39 -0400

    stuck on crontrigger export default syntax for scheduled

--- fus_api/src/crontrigger.ts
+++ fus_api/src/crontrigger.ts
@@ -4,7 +4,7 @@ export interface Env {
 	QUANTUM_NUMBERS_API_KEY: string;
 	QUANTUM_NUMBERS_URL: string;
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	QRANDOMNESS_KV: KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -28,63 +28,79 @@ const corsHeaders = {
 // const storeBody = () => {
 
 // 	}
-// 	async function handleBatchAPI(env: Env, request: Request): Promise<Response> {
-//   // const {searchParams} = new URL(request.url);
-//   // let outcomes = searchParams.getAll('outcome');
-//   // const max = Number(searchParams.get('max'));
-//   // if (max > 0) {
-//   //   for (let i = 1; i <= max; i++) {
-//   //     outcomes.push(i.toString());
-//   //   }
-//   // }
-//   // if (outcomes.length === 0) {
-//   //   outcomes = defaultOutcomes;
-//   // }
-//   const qrngResult = await fetch(env.QUANTUM_NUMBERS_URL + "?length=1024&type=hex16&size=10", {
-//     headers: {
-//       'x-api-key': env.QUANTUM_NUMBERS_API_KEY
-//     }
-//   });
-//   if (qrngResult.status != 200) {
-//     const responseHeaders = new Headers(qrngResult.headers);
-//     responseHeaders.set('Access-Control-Allow-Origin', allowedOrigin);
-// 		//Add code for body
-//     return new Response(qrngResult.body, {
-//         headers: responseHeaders,
-//         status: qrngResult.status,
-//         statusText: qrngResult.statusText
-//     });
-// 		//TODO: Store Response in the KV
-//   } else{
+	async function handleRequest(env: Env, request: Request): Promise<Response> {
 
-// 		return new Response(
+  const qrngResult = await fetch(env.QUANTUM_NUMBERS_URL + "?length=1024&type=hex16&size=10", {
+    headers: {
+      'x-api-key': env.QUANTUM_NUMBERS_API_KEY
+    }
+  });
+  if (qrngResult.status != 200) {
+    const responseHeaders = new Headers(qrngResult.headers);
+    responseHeaders.set('Access-Control-Allow-Origin', allowedOrigin);
+		//Add code for body
+    return new Response(qrngResult.body, {
+        headers: responseHeaders,
+        status: qrngResult.status,
+        statusText: qrngResult.statusText
+    });
 
-// 			'no randomness', {status: 404}
-// 		)
-// 		}
-// 	}
-// 	// else {
 
-// 	// 	//TODO: Properly type json
-//   //   const json: any = await qrngResult.json();
-//   //   const randNum = json.data[0];
-//   //   const selectedOutcome = outcomes[randNum % outcomes.length];
-//   //   const p = 1.0/outcomes.length;
-//   //   const responseHeaders = new Headers();
-//   //   responseHeaders.set('Access-Control-Allow-Origin', allowedOrigin);
-//   //   responseHeaders.set('Content-Type', 'text/plain')
-//   //   return new Response(`${selectedOutcome}`, {
-//   //       headers: responseHeaders,
-//   //       status: 200,
-//   //       statusText: selectedOutcome
-//   //   });
-//   // }
-// // }
+
+  } else{
+
+		return new Response(
+
+			'no randomness', {status: 404}
+		)
+		}
+	}
+
+
+	//gather Results
+	async function handleResults(env: Env, request: Request): Promise<Response> {
+		const QRCONTENT = await handleRequest(env, request);
+		const QRDATA = await JSON.stringify(QRCONTENT.json());
+
+		await env.QRANDOMNESS_KV.put("qRandomness", QRDATA)
+
+		return new Response(
+			'success set KV', {status: 200}
+		)
+
+	}
+
+		function handleOptions(request: Request): Response {
+    const respHeaders = {
+      ...corsHeaders,
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+			//TODO: Removed for debug
+    //   'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers'),
+    };
+    return new Response(null, {
+      headers: respHeaders,
+    });
+}
+
 
 const handler: ExportedHandler = {
-  async scheduled(controller, env, ctx) {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext){
+		if (request.method === 'OPTIONS') {
+				return handleOptions(request);
+		} else if (request.method === 'GET') {
+			return handleRequest(env, request);
+		}
+		return new Response(
+			'not found', {status: 404}
+		)
+		},
 
-		console.log("cron processed")
+
+  async scheduled(request: Request, env: Env, ctx: ExecutionContext) {
+
+		ctx.waitUntil(handleResults(env, request))
+
   },
 };
-export default handler;
+export default handler
